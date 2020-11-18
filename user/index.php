ad3fbf4716f6f9edbf8d66f536f3edd8ed4364ce
wata <watap1012@gmail.com>
2020-11-18 21:43:02 +0900

    1-4userlogin

--- user/index.php
+++ user/index.php
@@ -1,2 +1,54 @@
+<!DOCTYPE html>
+<html lang="ja">
 <?php
-echo "ユーザー登録ページ!!";
+include_once "../common/header.php";
+echo getHeader("ユーザー登録");
+ ?>
+
+  <body>
+    <div class="d-flex align-items-center justify-content-center h-100">
+      <form method="post" action="../memo/">
+        <div class="card rounded login-card-width shadow">
+          <div class="card-body">
+            <div class="rounded-circle mx-auto border-gray border d-flex mt-3 icon-circle">
+              <img src="../public/images/animal_stand_zou.png" alt="loginicon" class="w-75 mx-auto p-2">
+            </div>
+            <div class="d-flex justify-content-center">
+              <div class="mt-3 h2">SimpleMemo</div>
+            </div>
+            <div class="row mt-3">
+              <div class="offset-2 col-8 offset-2">
+                <label for="" class="input-group w-100">
+                  <span class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="fas fa-file-signature"></i>
+                    </span>
+                  </span>
+                  <input type="text" name="user_name" class="form-control" placeholder="ユーザー名" autocomplete="off" maxlength="255">
+                </label>
+                <label for="" class="input-group w-100">
+                  <span class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="far fa-envelope"></i>
+                    </span>
+                  </span>
+                  <input type="text" name="user_email" class="form-control" placeholder="メールアドレス" autocomplete="off" maxlength="255">
+                </label>
+                <label for="" class="input-group w-100">
+                  <span class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="fas fa-key"></i>
+                    </span>
+                  </span>
+                  <input type="password" name="user_password" class="form-control" placeholder="パスワード" autocomplete="off">
+                </label>
+                <button class="form-control btn btn-success">登録する</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </body>
+
+</html>
